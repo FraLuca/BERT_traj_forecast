d1fb4dc89f46f00ab2ecea9b65d08ff6ea350300
Alessandro Flaborea <flaborea.alessandro@gmail.com>
2021-02-18 16:26:05 +0100

    indentation

--- shopper/baselineUtils.py
+++ shopper/baselineUtils.py
@@ -14,84 +14,84 @@ import random
 
 
 class IndividualTfDataset(Dataset):
-    def __init__(self,data):
-        super(IndividualTfDataset,self).__init__()
+	def __init__(self,data):
+		super(IndividualTfDataset,self).__init__()
 
-        self.data=data
+		self.data=data
 
-    def __len__(self):
-        return self.data['src'].shape[0]
+	def __len__(self):
+		return self.data['src'].shape[0]
 
 
-    def __getitem__(self,index):
-        return {'src':torch.Tensor(self.data['src'][index]),
-                'trg':torch.Tensor(self.data['trg'][index]),
-                'tag_id':self.data['tag_id'][index],
-                'seq_idx':self.data['seq_idx'][index],
-                }
+	def __getitem__(self,index):
+		return {'src':torch.Tensor(self.data['src'][index]),
+				'trg':torch.Tensor(self.data['trg'][index]),
+				'tag_id':self.data['tag_id'][index],
+				'seq_idx':self.data['seq_idx'][index],
+				}
 
 def create_dataset(full_df, n_obs=40, n_preds=60):
-    
-    data_pos=[]
-    data_speed=[]
-    data_rel_pos=[]
-    
-    info_tag_id=[]
-    info_seq_idx=[]
-    
-    data = {}
-    
-    splitted_df = {n: full_df.iloc[n:n+100, :] 
-           for n in range(0, len(full_df), 100)}
+	
+	data_pos=[]
+	data_speed=[]
+	data_rel_pos=[]
+	
+	info_tag_id=[]
+	info_seq_idx=[]
+	
+	data = {}
+	
+	splitted_df = {n: full_df.iloc[n:n+100, :] 
+		   for n in range(0, len(full_df), 100)}
 
-    info_tag_id = list(map(lambda kv: kv[1]['tag_id'].iloc[0], splitted_df.items()))
-    info_seq_idx = list(map(lambda kv: kv[1]['seq_idx'].iloc[0], splitted_df.items()))
+	info_tag_id = list(map(lambda kv: kv[1]['tag_id'].iloc[0], splitted_df.items()))
+	info_seq_idx = list(map(lambda kv: kv[1]['seq_idx'].iloc[0], splitted_df.items()))
 
 
-    data_pos = list(map(lambda kv: kv[1][['x','y']].values.astype(np.float32), splitted_df.items()))
-    data_speed = list(map(lambda kv: kv[1][['x','y']].diff().values.astype(np.float32), splitted_df.items()))
-    data_rel_pos = list(map(lambda kv: (kv[1][['x','y']] - kv[1][['x','y']].iloc[0,:]).values.astype(np.float32), splitted_df.items()))                
-    
-    data_pos_stack = np.stack(data_pos)
-    data_speed_stack = np.stack(data_speed)
-    data_rel_pos_stack = np.stack(data_rel_pos)
-    info_tag_id_stack = np.stack(info_tag_id)
-    info_seq_idx_stack = np.stack(info_seq_idx)
-    
-    all_data = np.concatenate((data_pos_stack, data_speed_stack, data_rel_pos_stack), 2)
-    inp = all_data[:,:n_obs,:]
-    out = all_data[:,n_obs:,:]
-    
-    data['src'] = inp
-    data['trg'] = out
-    data['tag_id'] = info_tag_id_stack
-    data['seq_idx'] = info_seq_idx_stack
-    
-    return IndividualTfDataset(data)
+	data_pos = list(map(lambda kv: kv[1][['x','y']].values.astype(np.float32), splitted_df.items()))
+	data_speed = list(map(lambda kv: kv[1][['x','y']].diff().values.astype(np.float32), splitted_df.items()))
+	data_rel_pos = list(map(lambda kv: (kv[1][['x','y']] - kv[1][['x','y']].iloc[0,:]).values.astype(np.float32), splitted_df.items()))                
+	
+	data_pos_stack = np.stack(data_pos)
+	data_speed_stack = np.stack(data_speed)
+	data_rel_pos_stack = np.stack(data_rel_pos)
+	info_tag_id_stack = np.stack(info_tag_id)
+	info_seq_idx_stack = np.stack(info_seq_idx)
+	
+	all_data = np.concatenate((data_pos_stack, data_speed_stack, data_rel_pos_stack), 2)
+	inp = all_data[:,:n_obs,:]
+	out = all_data[:,n_obs:,:]
+	
+	data['src'] = inp
+	data['trg'] = out
+	data['tag_id'] = info_tag_id_stack
+	data['seq_idx'] = info_seq_idx_stack
+	
+	return IndividualTfDataset(data)
 
 
 
 def distance_metrics(gt,preds):
-    errors = np.zeros(preds.shape[:-1])
-    for i in range(errors.shape[0]):
-        for j in range(errors.shape[1]):
-            errors[i, j] = scipy.spatial.distance.euclidean(gt[i, j], preds[i, j])
-    return errors.mean(),errors[:,-1].mean(),errors
+	errors = np.zeros(preds.shape[:-1])
+	for i in range(errors.shape[0]):
+		for j in range(errors.shape[1]):
+			errors[i, j] = scipy.spatial.distance.euclidean(gt[i, j], preds[i, j])
+	return errors.mean(),errors[:,-1].mean(),errors
 
 
 
 if __name__=='__main__':
 
-    parser=argparse.ArgumentParser(description='Preprocess the Shopper dataset')
-    parser.add_argument('--max_number_files',type=int, default=None)  
+	parser=argparse.ArgumentParser(description='Preprocess the Shopper dataset')
+	parser.add_argument('--max_number_files',type=int, default=None)  
 
-    args=parser.parse_args()
+	args=parser.parse_args()
 
 	files = os.listdir('./preprocessed')
 	full_df = pd.DataFrame(columns=['x', 'y', 'tag_id', 'seq_idx'])
 	for f in files[:args.max_number_files]:
-	    curr_df = pd.read_csv(os.path.join('./preprocessed', f))
-	    full_df = pd.concat([full_df, curr_df])
+		curr_df = pd.read_csv(os.path.join('./preprocessed', f))
+		full_df = pd.concat([full_df, curr_df])
 
 	
 	train_dataset = create_dataset(full_df)
--- shopper/preprocessing.py
+++ shopper/preprocessing.py
@@ -13,155 +13,155 @@ from tqdm.auto import tqdm
 
 
 def cast_time(x):
-    return datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S')
+		return datetime.datetime.strptime(x,'%Y-%m-%d %H:%M:%S')
 
 def compute_delta_time(x):
-    date_1 = sub_df.iloc[0]['datetime']
-    date_2 = x
-    time_delta = (date_2 - date_1)
-    return time_delta.total_seconds()
+		date_1 = sub_df.iloc[0]['datetime']
+		date_2 = x
+		time_delta = (date_2 - date_1)
+		return time_delta.total_seconds()
 
 
 
 def extract_sequences(threshold, min_seq_time_length, min_num_points, df):
-    
-    # threshold = posizioni più distanti in tempo di questa threshold spezzano la sequenza
-    # min_seq_time_length = selezioniaimo sequenze t.c. il punto finale e iniziale distano almeno min_seq_time_length secondi 
-    # min_num_points = selezioniaimo sequenze t.c. il numero di punti sia almeno min_num_points
-    
-    curr_seq = []
-    seq_idx = 0
+		
+		# threshold = posizioni più distanti in tempo di questa threshold spezzano la sequenza
+		# min_seq_time_length = selezioniaimo sequenze t.c. il punto finale e iniziale distano almeno min_seq_time_length secondi 
+		# min_num_points = selezioniaimo sequenze t.c. il numero di punti sia almeno min_num_points
+		
+		curr_seq = []
+		seq_idx = 0
 
-    new_seq_df = pd.DataFrame(columns = ['tag_id', 'time', 'x', 'y', 'description', 'datetime', 'deltatime', 'seq_idx'])
+		new_seq_df = pd.DataFrame(columns = ['tag_id', 'time', 'x', 'y', 'description', 'datetime', 'deltatime', 'seq_idx'])
 
-    
-    for tag_id in tqdm(df['tag_id'].unique()):
-        
-        sub_df = df[df['tag_id'] == tag_id]
-        sub_df = sub_df.sort_values(by='time')
-        sub_df['datetime'] = sub_df['time'].apply(cast_time)
-        sub_df['deltatime'] = sub_df['datetime'].apply(compute_delta_time)
-    
-        for row in range(len(sub_df)-1):
-            if (sub_df.iloc[row+1]['deltatime'] - sub_df.iloc[row]['deltatime']) <= threshold:
-                if len(curr_seq) == 0:
-                    curr_seq.append(sub_df.iloc[row])
-                    curr_seq.append(sub_df.iloc[row+1])
-                else:
-                    curr_seq.append(sub_df.iloc[row+1])
-            else:
-                if len(curr_seq) >= min_num_points:
-                    #list_of_seq.append(curr_seq)
-                    out_df = pd.DataFrame(curr_seq)
-                    if (out_df.iloc[-1]['deltatime'] - out_df.iloc[0]['deltatime']) >= min_seq_time_length:
-                        out_df = out_df.drop_duplicates(subset=['datetime'])
-                        out_df['seq_idx'] = [seq_idx]*len(out_df)
-                        seq_idx += 1
-                        new_seq_df = pd.concat([new_seq_df, out_df], ignore_index=True, sort=False)
+		
+		for tag_id in tqdm(df['tag_id'].unique()):
+				
+				sub_df = df[df['tag_id'] == tag_id]
+				sub_df = sub_df.sort_values(by='time')
+				sub_df['datetime'] = sub_df['time'].apply(cast_time)
+				sub_df['deltatime'] = sub_df['datetime'].apply(compute_delta_time)
+		
+				for row in range(len(sub_df)-1):
+						if (sub_df.iloc[row+1]['deltatime'] - sub_df.iloc[row]['deltatime']) <= threshold:
+								if len(curr_seq) == 0:
+										curr_seq.append(sub_df.iloc[row])
+										curr_seq.append(sub_df.iloc[row+1])
+								else:
+										curr_seq.append(sub_df.iloc[row+1])
+						else:
+								if len(curr_seq) >= min_num_points:
+										#list_of_seq.append(curr_seq)
+										out_df = pd.DataFrame(curr_seq)
+										if (out_df.iloc[-1]['deltatime'] - out_df.iloc[0]['deltatime']) >= min_seq_time_length:
+												out_df = out_df.drop_duplicates(subset=['datetime'])
+												out_df['seq_idx'] = [seq_idx]*len(out_df)
+												seq_idx += 1
+												new_seq_df = pd.concat([new_seq_df, out_df], ignore_index=True, sort=False)
 
-                curr_seq = []
-            
-    return new_seq_df
+								curr_seq = []
+						
+		return new_seq_df
 
 
 def pad_missing_values(curr_seq):
-    curr_seq['deltatime'] = curr_seq['deltatime'] - curr_seq['deltatime'].iloc[0]
-    last_timestamp = int(curr_seq['deltatime'].iloc[-1])+1
-    filled_seq = dict(zip(list(range(last_timestamp)), [float("NAN")]*last_timestamp))
-    for row in range(len(curr_seq)):
-        filled_seq[curr_seq['deltatime'].iloc[row]] = curr_seq[['x','y']].iloc[row]
-    filled_df = pd.DataFrame.from_dict(filled_seq).T    
-    return filled_df.astype(np.float32)
+		curr_seq['deltatime'] = curr_seq['deltatime'] - curr_seq['deltatime'].iloc[0]
+		last_timestamp = int(curr_seq['deltatime'].iloc[-1])+1
+		filled_seq = dict(zip(list(range(last_timestamp)), [float("NAN")]*last_timestamp))
+		for row in range(len(curr_seq)):
+				filled_seq[curr_seq['deltatime'].iloc[row]] = curr_seq[['x','y']].iloc[row]
+		filled_df = pd.DataFrame.from_dict(filled_seq).T    
+		return filled_df.astype(np.float32)
 
 
 def sequence_creator(filled_curr_seq, tag_id, seq_idx, threshold=75, stride=5, len_seq=100):
-    
-    filled_df = filled_curr_seq.interpolate(method='ffill', axis=0)
+		
+		filled_df = filled_curr_seq.interpolate(method='ffill', axis=0)
 
-    all_sequences = []
-    accepted_seq = 0
-    
-    if len(filled_df)<threshold:
-        return pd.DataFrame(columns=['x', 'y', 'tag_id', 'seq_idx'])
-    elif len(filled_df) <= len_seq:
-        # Pad at the begginning and add to list
-        df_nan = pd.DataFrame(np.nan, index=np.arange(100-(len(filled_df)+1)), columns=['x', 'y'])
-        if len(filled_df) != len_seq:
-            new_df = pd.concat([filled_df.head(1), df_nan, filled_df])
-        else: 
-            new_df = filled_df
-        # Add seq_idx
-        new_seq_idx = str(seq_idx)+'_'+str(accepted_seq)
-        accepted_seq += 1
-        new_df['seq_idx'] = np.repeat([new_seq_idx], len_seq)
+		all_sequences = []
+		accepted_seq = 0
+		
+		if len(filled_df)<threshold:
+				return pd.DataFrame(columns=['x', 'y', 'tag_id', 'seq_idx'])
+		elif len(filled_df) <= len_seq:
+				# Pad at the begginning and add to list
+				df_nan = pd.DataFrame(np.nan, index=np.arange(100-(len(filled_df)+1)), columns=['x', 'y'])
+				if len(filled_df) != len_seq:
+						new_df = pd.concat([filled_df.head(1), df_nan, filled_df])
+				else: 
+						new_df = filled_df
+				# Add seq_idx
+				new_seq_idx = str(seq_idx)+'_'+str(accepted_seq)
+				accepted_seq += 1
+				new_df['seq_idx'] = np.repeat([new_seq_idx], len_seq)
 
-        # Add tag_id
-        new_df['tag_id'] = np.repeat([tag_id], len_seq)
+				# Add tag_id
+				new_df['tag_id'] = np.repeat([tag_id], len_seq)
 
-        all_sequences.append(new_df)
-    elif len(filled_df) > len_seq:
-        for i in range(1+(len(filled_df) - len_seq) // stride):
-            new_df = filled_df.iloc[i*stride : i*stride+len_seq, [0,1]].reset_index(drop=True)
+				all_sequences.append(new_df)
+		elif len(filled_df) > len_seq:
+				for i in range(1+(len(filled_df) - len_seq) // stride):
+						new_df = filled_df.iloc[i*stride : i*stride+len_seq, [0,1]].reset_index(drop=True)
 
-            # Add seq_idx
-            new_seq_idx = str(seq_idx)+'_'+str(accepted_seq)
-            accepted_seq += 1
-            try:
-                new_df['seq_idx'] = np.repeat([new_seq_idx], len_seq)
-            except:
-                print(len(new_seq_idx), len_seq)
-            # Add tag_id
-            new_df['tag_id'] = np.repeat([tag_id], len_seq)
+						# Add seq_idx
+						new_seq_idx = str(seq_idx)+'_'+str(accepted_seq)
+						accepted_seq += 1
+						try:
+								new_df['seq_idx'] = np.repeat([new_seq_idx], len_seq)
+						except:
+								print(len(new_seq_idx), len_seq)
+						# Add tag_id
+						new_df['tag_id'] = np.repeat([tag_id], len_seq)
 
-            all_sequences.append(new_df)
-    if len(all_sequences) > 0:
-        return pd.concat(all_sequences).reset_index(drop=True)
-    else:
-        return pd.DataFrame(columns=['x', 'y', 'tag_id', 'seq_idx'])
+						all_sequences.append(new_df)
+		if len(all_sequences) > 0:
+				return pd.concat(all_sequences).reset_index(drop=True)
+		else:
+				return pd.DataFrame(columns=['x', 'y', 'tag_id', 'seq_idx'])
 
 
 def general_function(sub_list):
-    
-    sssseq = []
-    splitted_df = {seq_idx: seq_df[seq_df['seq_idx']==seq_idx] for seq_idx in sub_list}
+		
+		sssseq = []
+		splitted_df = {seq_idx: seq_df[seq_df['seq_idx']==seq_idx] for seq_idx in sub_list}
 
-    prova = list(map(lambda kv: sequence_creator(pad_missing_values(kv[1]), kv[1]['tag_id'].iloc[0], kv[1]['seq_idx'].iloc[0]), splitted_df.items()))
-    sssseq += prova
-    final_df = pd.concat(sssseq).reset_index(drop=True)
-    final_df.to_csv("./preprocessed/sequences_from_"+str(sub_list[0])+"_to_"+str(sub_list[-1])+".csv")
+		prova = list(map(lambda kv: sequence_creator(pad_missing_values(kv[1]), kv[1]['tag_id'].iloc[0], kv[1]['seq_idx'].iloc[0]), splitted_df.items()))
+		sssseq += prova
+		final_df = pd.concat(sssseq).reset_index(drop=True)
+		final_df.to_csv("./preprocessed/sequences_from_"+str(sub_list[0])+"_to_"+str(sub_list[-1])+".csv")
 
 
 
 
 if __name__=='__main__':
 
-    parser=argparse.ArgumentParser(description='Preprocess the Shopper dataset')
-    parser.add_argument('--dataset_file',type=str)  # edeka, aldi, globus, rewe 
-    parser.add_argument('--dataset_folder',type=str, default='raw_trajectories') 
-    parser.add_argument('--preprocess_part',type=int, default=2)  
+		parser=argparse.ArgumentParser(description='Preprocess the Shopper dataset')
+		parser.add_argument('--dataset_file',type=str)  # edeka, aldi, globus, rewe 
+		parser.add_argument('--dataset_folder',type=str, default='raw_trajectories') 
+		parser.add_argument('--preprocess_part',type=int, default=2)  
 
-    args=parser.parse_args()
+		args=parser.parse_args()
 
-    outdir=f'preprocessed'
-    try:
-        os.mkdir(outdir)
-    except:
-        pass
+		outdir=f'preprocessed'
+		try:
+				os.mkdir(outdir)
+		except:
+				pass
 
-    if args.preprocess_part == 1:
+		if args.preprocess_part == 1:
 
-	    df = pd.read_csv('./'+args.dataset_folder+'/'+args.dataset_file+'_dataset.txt', sep=';')
-	    seq_df = extract_sequences(dataset = df, threshold=120, min_seq_time_length=10, min_num_points=5)
-	    seq_df.to_csv('./'+args.dataset_folder+'/'+args.dataset_file+'_extracted_sequences.csv', index=False) 
- 	
- 	else:
+			df = pd.read_csv('./'+args.dataset_folder+'/'+args.dataset_file+'_dataset.txt', sep=';')
+			seq_df = extract_sequences(dataset = df, threshold=120, min_seq_time_length=10, min_num_points=5)
+			seq_df.to_csv('./'+args.dataset_folder+'/'+args.dataset_file+'_extracted_sequences.csv', index=False) 
+	
+		else:
 
- 		seq_df = pd.read_csv('./'+args.dataset_folder+'/'+args.dataset_file+'_extracted_sequences.csv')
- 	
- 		len_processed_seq = 200
-		tot_seq = len(seq_df['seq_idx'].unique()) # //5 # Per ora ne processiamo un quinto
-		inputs = [np.arange(i*len_processed_seq, min((i+1)*len_processed_seq, tot_seq)) for i in range(tot_seq//len_processed_seq + 1)]
+			seq_df = pd.read_csv('./'+args.dataset_folder+'/'+args.dataset_file+'_extracted_sequences.csv')
+		
+			len_processed_seq = 200
+			tot_seq = len(seq_df['seq_idx'].unique()) # //5 # Per ora ne processiamo un quinto
+			inputs = [np.arange(i*len_processed_seq, min((i+1)*len_processed_seq, tot_seq)) for i in range(tot_seq//len_processed_seq + 1)]
 
 
-		num_cores = multiprocessing.cpu_count() - 2
-		results = Parallel(n_jobs=num_cores)(delayed(general_function)(i) for i in inputs)
+			num_cores = multiprocessing.cpu_count() - 2
+			results = Parallel(n_jobs=num_cores)(delayed(general_function)(i) for i in inputs)
